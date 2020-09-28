2f52b6dae0bad4c817b7e91a1ae0ff0edd3e2cd9
Pruthvisinh Rajput <61496236+impruthvi@users.noreply.github.com>
2020-09-28 20:07:10 +0530

    Reset viewer
    Now you can reset viewers.

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -171,7 +171,7 @@
                                        echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                                        echo "<td> <a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                                        echo "<td> <a onClick=\" javascript: return confirm('Are you sure you want to delete'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
-                                       echo "<td>$post_views_count</td>";
+                                       echo "<td><a href='posts.php?reset={$post_id}'>$post_views_count</a></td>";
                                    
                                    
                                    echo "</tr>";
@@ -193,6 +193,14 @@
         
         $query = "DELETE FROM postes WHERE post_id = {$the_post_id} ";
         $delete_query = mysqli_query($conection,$query);
+        header("Location: posts.php");
+    }
+
+   if(isset($_GET['reset'])){
+        $the_post_id = $_GET['reset'];
+        
+        $query = "UPDATE postes SET post_views_count = 0 WHERE post_id =". mysqli_real_escape_string($conection,$_GET['reset']) . " ";
+        $reset_query = mysqli_query($conection,$query);
         header("Location: posts.php");
     }
 
