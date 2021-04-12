75d480c0fbe481f38cb2b03bff05b940f18d90ea
Pruthvi <pruthvirajput97@gmail.com>
2021-04-12 13:01:07 +0530

    Done👍

---
+++ README.md
@@ -0,0 +1,78 @@
+# **CMS (Control Management System)**
+
+## **Systerm Functions**
+
+### User Side
+
+---
+
+- View the Blogs
+- See Blog by category
+- Filter Blog by author name
+- Search Blog
+- Add comments on Blog
+- Login User
+
+### Admin side
+
+---
+
+- Dashbord
+  - You can see all the details about website.
+  - You can see the graphical representation of website activities
+
+* Blogs
+
+  - View all Blogs
+  - Add Blogs
+  - Edit Blogs
+  - Clone Blogs
+  - Delete Blogs
+  - Draft Blogs
+  - Publish Blogs
+
+* Category
+
+  - View All Category
+  - Add Category
+  - Delete Category
+  - Edit Category
+
+* Comments
+  - View All Comments
+  - Delete Comments
+  - Approve / Unapprove Comments
+* User
+  - View All Users
+  - Add User
+  - Delete User
+  - Edit User
+  - Change Users Role(Admin/Subscriber)
+* Profile
+  - View Your's profile
+  - Edit your's profile
+  - Change Password
+* You can also see, how many people are see your websire(Online)
+* Only admin can login in the admin panel
+
+* All screenshots are in screenshots folder
+
+## **Installation Steps**
+
+1. Download the zip file and Unzip the file on your local server.
+
+2. Put this file inside "c:/xampp/htdocs/"
+
+3. Database Configuration Open PHPMyAdmin Create a Database named cms. Import database cms.sql from the downloaded folder(inside the database)
+
+4. Open Your browser put inside "http://localhost/CMS/"
+
+5. To Login as admin put "http://localhost/CMS/admin"
+
+## **Admin Login Details**
+
+- Login Id: impruthvi
+
+- Password: impruthvi
+
+![Pruthvi's GitHub stats](https://github-readme-stats.vercel.app/api?username=impruthvi&show_icons=true&theme=dark)
--- database/cms.sql
+++ database/cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 29, 2020 at 10:12 AM
+-- Generation Time: Apr 12, 2021 at 07:21 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -41,7 +41,8 @@ INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 (2, 'Java'),
 (3, 'Python'),
 (4, 'Javascript'),
-(29, 'CMS');
+(29, 'CMS'),
+(31, 'HTML');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,8 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_id`, `comment_post_id`, `comment_author`, `comment_email`, `comment_content`, `comment_status`, `comment_date`) VALUES
-(5, 7, 'Pruthvi', 'pruthvi@gmail.com', 'Hello\r\n', 'approve', '2020-08-28');
+(17, 21, 'asdasd', 'asdasd@sdfdds.com', 'dsfsdfsfsfsdfsdf', 'approve', '2020-10-03'),
+(19, 21, 'dasdas', 'ximejo6984@lege4h.com', 'asdasd', 'unapprove', '2021-03-17');
 
 -- --------------------------------------------------------
 
@@ -92,7 +94,66 @@ CREATE TABLE `postes` (
 --
 
 INSERT INTO `postes` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`, `post_user`, `post_views_count`) VALUES
-(10, 1, 'Javascript', 'Adwin', '2020-08-29', 'javascript.jpg', '', 'good work', 0, 'published', '', 0);
+(47, 4, 'Javascript', '', '2021-04-12', 'test.png', '', '<p>Testing the add post</p>', 0, 'published', 'impruthvi', 10),
+(48, 3, 'Pyhton Post', '', '2021-04-12', 'python.jpg', '', '<p>Done👍</p><p>&nbsp;</p>', 0, 'published', 'impruthvi', 1),
+(49, 31, 'HTML Post', '', '2021-04-12', 'html.png', '', '', 0, 'published', 'pruthviii', 1),
+(50, 4, 'Javascript', '', '2021-04-12', 'test.png', '', '<p>Testing the add post</p>', 0, 'published', 'impruthvi', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(3) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_firstname` varchar(255) NOT NULL,
+  `user_lastname` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_image` text NOT NULL,
+  `user_role` varchar(255) NOT NULL,
+  `randSalt` varchar(255) NOT NULL DEFAULT '$2y$10$iusesomecrazystrings22'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_firstname`, `user_lastname`, `user_email`, `user_image`, `user_role`, `randSalt`) VALUES
+(36, 'impruthvi', '$2y$12$hlvrYMuLBWeKX9mDI5dgS.jB7YRshnMOCo2/PKMQ99OXs0/E2pOvy', '', '', 'impruthvi@gmail.com', '', 'admin', '$2y$10$iusesomecrazystrings22'),
+(37, 'a', '$2y$12$e/kGRiXb0iAqDYs2hINHnuKoNJQRBoSxP9E.0NAmtG35mVz9GzIxe', '', '', 'a@a', '', 'admin', '$2y$10$iusesomecrazystrings22'),
+(38, 'pruthviii', '$2y$12$3N.bvtBO4i0T2Ys416hdEurHEn4RrAQnmHo.vNiOTHwIrn3bF.ePi', '', '', 'p@p', '', 'admin', '$2y$10$iusesomecrazystrings22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_online`
+--
+
+CREATE TABLE `users_online` (
+  `id` int(11) NOT NULL,
+  `session` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users_online`
+--
+
+INSERT INTO `users_online` (`id`, `session`, `time`) VALUES
+(4, 'cmnt54mnok2nqa2850a82n7c9k', 1601488213),
+(5, '6br2jfnjoohujkk45nhtjrf82m', 1601488295),
+(6, 'euuqpdf5u3sigcfk71hccch0ti', 1601571839),
+(7, '6q1o9kqe1l4iqpgn7idcvff1le', 1603906965),
+(8, 'sibvmg3vdtqe0l10pl2c4sjl66', 1601826947),
+(9, 'j18j4nlpans8tmq4k5cn4iabm4', 1606571147),
+(10, 'sni12336a78h9sen1jubr72q2l', 1609342887),
+(11, 'otj3s2cagao1csud9bioonhmei', 1615714349),
+(12, 'fn611t1vvup0nbmeghfv6hctg1', 1615921091),
+(13, 'kemapg89pa3ito3trrj75kk2qa', 1616069983),
+(14, 'l4ucg07od59aodhjh9a9rjvd9d', 1618204652);
 
 --
 -- Indexes for dumped tables
@@ -117,6 +178,18 @@ ALTER TABLE `postes`
   ADD PRIMARY KEY (`post_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `users_online`
+--
+ALTER TABLE `users_online`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -124,19 +197,31 @@ ALTER TABLE `postes`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `cat_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `postes`
 --
 ALTER TABLE `postes`
-  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `users_online`
+--
+ALTER TABLE `users_online`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
